302df94486e00222081544a3af82c060f3bf7b94
Antonio1885 <anthonyecheverria945@gmail.com>
2025-06-24 22:53:36 -0500

    Add files via upload

---
+++ piedr, papel y tijeras corregido.py
@@ -0,0 +1,44 @@
+
+import random
+
+
+def jugar():
+  while True:
+    jugador = input("ingrese (papel, piedra,  tijeras) o ¨salir¨ para dejar de jugar: ").strip().lower()
+    if jugador =="salir":
+        print("gracias por jugar ")
+        break
+    
+    elegir= ("papel", "piedra",  "tijeras")
+    computadora = random.choice(elegir)
+    print(f"tu eliges {jugador}, computador elige,{computadora}")
+   
+    if jugador == computadora:
+     print("empate")
+
+    elif jugador == "piedra":
+       if computadora == "tijeras":
+          print("ganaste")
+        
+       else:
+            print ("perdiste ") 
+
+    elif jugador == "papel":
+        if computadora == " piedra ":          
+            print("ganaste")
+       
+        else:
+            print("perdiste ")    
+
+    elif jugador == "tijeras":
+         if computadora == "papel":
+            print("ganaste")
+       
+         else:
+           print("perdiste")
+    else:
+      print("Entrada no válida. Por favor, elige papel, piedra o tijeras")             
+                      
+     
+             
+jugar()
